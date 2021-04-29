4ffcc65178222561ab6d0b275fc75b784bf4c620
Jeewoo Yoon <jeewooyoon@Jeewooui-MacBookPro-2.local>
2021-04-29 16:22:41 +0900

    project build

---
+++ sketch.js
@@ -0,0 +1,53 @@
+let handpose;
+let video;
+let predictions = [];
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  handpose = ml5.handpose(video, modelReady);
+
+  // This sets up an event that fills the global variable "predictions"
+  // with an array every time new hand poses are detected
+  handpose.on("predict", results => {
+    predictions = results;
+  });
+
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  console.log("Model ready!");
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  //move image by the width of image to the left
+  translate(video.width, 0);
+  //then scale it by -1 in the x-axis
+  //to flip the image
+  scale(-1, 1);
+  //draw video capture feed as image inside p5 canvas
+  image(video, 0, 0);
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+
+  // console.log(predictions);
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  for (let i = 0; i < predictions.length; i += 1) {
+    const prediction = predictions[i];
+    for (let j = 0; j < prediction.landmarks.length; j += 1) {
+      const keypoint = prediction.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      ellipse(keypoint[0], keypoint[1], 10, 10);
+    }
+  }
+}
